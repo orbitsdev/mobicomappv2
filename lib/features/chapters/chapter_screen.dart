d4b4a118392005a35cc2c9fe48a5956701b66941
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-03-15 08:35:46 +0800

    chane api lesson

--- lib/features/chapters/chapter_screen.dart
+++ lib/features/chapters/chapter_screen.dart
@@ -5,6 +5,7 @@ import 'package:mobicom/controllers/chapter_controller.dart';
 import 'package:mobicom/models/chapter.dart';
 import 'package:mobicom/models/lesson.dart';
 
+
 class ChapterScreen extends StatefulWidget {
   const ChapterScreen({Key? key}) : super(key: key);
   static String name = '/chapter-details';
@@ -32,6 +33,7 @@ class _ChapterScreenState extends State<ChapterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff111827),
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
@@ -53,23 +55,44 @@ class _ChapterScreenState extends State<ChapterScreen> {
                   // Render HTML content for chapter description
                   Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child: Html(
-                      data: chapter.description ?? '',
-                      style: {
-                        "img": Style(
-                          margin: Margins.symmetric(
-                              vertical: 10.0), // Adjust margin as needed
-                          width: Width(
-                              MediaQuery.of(context).size.width), // Set image width to screen width
-                          height: Height(200), // Set image height, adjust as needed
-                        ),
-                      },
+                    child: ExpansionTile(
+                      initiallyExpanded: true,
+                      title: Text("Chapter Overview"),
+                      children: [
+                         Html(
+                                          data: chapter.description ?? '',
+                        style: {
+                          "figcaption": Style(
+                            display: Display.none,
+                          ),
+                          "img": Style(
+                            margin: Margins.all(0),
+                            // Adjust margin as needed
+                            width: Width(300), // Set a fixed width for the image
+                            height:
+                                Height(200), // Set a fixed height for the image
+                            display: Display
+                                .block, // Ensure the image is displayed as a block element
+                            textAlign: TextAlign.start, // Center the image
+                          ),
+                          "pre": Style(
+                            border: Border.all(color: Colors.white),
+                            padding: HtmlPaddings.all(10),
+                            backgroundColor: Color(
+                                0xff0d121b), // Set image height, adjust as needed
+                          ),
+                        },
+                        
+                      )
+                      ],
                     ),
                   ),
                   // Render lesson titles
-                  ...chapterController.lessons.map((lesson) => ListTile(
-                        title: Text('${lesson.title}'),
-                      )),
+                 ...chapterController.lessons.map((lesson) => ListTile(
+  leading: lesson.image_path != null ? Image.network(lesson.image_path!) : null,
+  title: Text('${lesson.title}'),
+)),
+
                 ],
               ),
             ),
